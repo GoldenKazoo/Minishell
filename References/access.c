87b4353c102c5856b311767cd375da300d599f10
Zakarya <goldenkazoomail@gmail.com>
2024-12-21 18:32:10 +0100

    acces modified

--- References/access.c
+++ References/access.c
@@ -1,12 +1,22 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main()
-{
-    if (access("fichier.txt", F_OK) == 0) {
-        printf("Le fichier existe\n");
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv, char **envp) {
+    // Récupérer la valeur de la variable PATH
+    const char *path = getenv("PATH");
+
+    if (*envp == NULL)
+        printf("envp est null");
+    // Vérifier si la variable a été trouvée
+    if (path) {
+        printf("Le PATH est :\n%s\n", path);
     } else {
-        printf("Le fichier n'existe pas\n");
+        printf("La variable PATH n'est pas définie.\n");
     }
+
     return 0;
 }
+
