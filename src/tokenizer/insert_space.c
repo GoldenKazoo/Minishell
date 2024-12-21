a59e269267abdbcb9ff7439df363b622abf2a42f
Zakarya <goldenkazoomail@gmail.com>
2024-12-21 18:30:55 +0100

    Norm OK for inserting space in imput string

--- src/tokenizer/insert_space.c
+++ src/tokenizer/insert_space.c
@@ -1,156 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   insert_space.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zchagar <zchagar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 17:38:58 by zchagar           #+#    #+#             */
+/*   Updated: 2024/12/21 18:27:36 by zchagar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "tokenizer.h"
 
-size_t ft_count_space(char *prompt)
+int	ft_pipe_case(char *prompt, char *new_prompt, int *index, int state)
 {
-    int i;
-    int count;
-
-    i = 0;
-    count = 0;
-    while (prompt[i] != '\0')
-    {
-        if (prompt[i+1] && prompt[i-1])
-        {
-            if (prompt[i] == '|' && prompt[i-1] != ' ' && prompt[i+1] != ' ')
-            {
-                count = count + 2;
-            }
-            if (prompt[i] == '<' && prompt[i-1] != ' ' && prompt[i+1] != ' ')
-            {
-                count = count + 2;
-                if (prompt[i+1] == '<')
-                    i++;
-            }
-            if (prompt[i] == '>' && prompt[i-1] != ' ' && prompt[i+1] != ' ')
-            {
-                count = count + 2;
-                if (prompt[i+1] == '>')
-                    i++;
-            }
-        }
-        i++;
-    }
-    return (count);
+	if (!prompt || !new_prompt || !index[0] || !index[1])
+		return (0);
+	if (prompt[index[1] + 1] == '|')
+	{
+		state = 1;
+		new_prompt[index[0]] = prompt[index[1]];
+		index[0]++;
+		new_prompt[index[0]] = ' ';
+		index[0]++;
+		new_prompt[index[0]] = '|';
+		index[0]++;
+		new_prompt[index[0]] = ' ';
+		(index[1])++;
+	}
+	return (state);
 }
 
-int ft_pipe_case(char *prompt, char *new_prompt, int *i, int *j, int state)
+int	ft_right_redir_case(char *prompt, char *new_prompt, int *index, int state)
 {
-    if (!prompt || !new_prompt || !i || !j)
-        return (0);
-    if (prompt[*j + 1] == '|')
-    {
-        state = 1;
-        new_prompt[*i] = prompt[*j];
-        (*i)++;
-        new_prompt[*i] = ' ';
-        (*i)++;
-        new_prompt[*i] = '|';
-        (*i)++;
-        new_prompt[*i] = ' ';
-        (*j)++;
-    }
-    return (state);
+	if (!prompt || !new_prompt || !index[0] || !index[1])
+		return (0);
+	if (prompt[index[1] + 1] == '<')
+	{
+		state = 1;
+		new_prompt[index[0]++] = prompt[index[1]];
+		new_prompt[index[0]++] = ' ';
+		new_prompt[index[0]] = '<';
+		(index[1])++;
+		if (prompt[index[1] + 1] && prompt[index[1] + 1] == '<')
+		{
+			index[0]++;
+			new_prompt[index[0]++] = '<';
+			new_prompt[index[0]++] = ' ';
+			(index[1])++;
+		}
+		else
+		{
+			index[0]++;
+			new_prompt[index[0]] = ' ';
+		}
+	}
+	return (state);
 }
 
-int   ft_right_redir_case(char *prompt, char *new_prompt, int *i, int *j, int state)
+int	ft_left_redir_case(char *prompt, char *new_prompt, int *index, int state)
 {
-    if (!prompt || !new_prompt || !i || !j)
-        return (0);
-    if (prompt[*j + 1] == '<')
-    {
-        state = 1;
-        new_prompt[*i] = prompt[*j];
-        (*i)++;
-        new_prompt[*i] = ' ';
-        (*i)++;
-        new_prompt[*i] = '<';
-        (*j)++;
-        if (prompt[*j+1] && prompt[*j+1] == '<')
-        {
-            (*i)++;
-            new_prompt[*i] = '<';
-            (*i)++;
-            new_prompt[*i] = ' ';
-            (*j)++;
-        }
-        else
-        {
-            (*i)++;
-            new_prompt[*i] = ' ';
-        }
-    }
-    return (state);
+	if (!prompt || !new_prompt || !index[0] || !index[1])
+		return (0);
+	if (prompt[index[1] + 1] == '>')
+	{
+		state = 1;
+		new_prompt[index[0]++] = prompt[index[1]];
+		new_prompt[index[0]++] = ' ';
+		new_prompt[index[0]++] = '>';
+		(index[1])++;
+		if (prompt[index[1] + 1] && prompt[index[1] + 1] == '>')
+		{
+			index[0]++;
+			new_prompt[index[0]++] = '>';
+			new_prompt[index[0]] = ' ';
+			(index[1])++;
+		}
+		else
+		{
+			index[0]++;
+			new_prompt[index[0]] = ' ';
+		}
+	}
+	return (state);
 }
 
-int    ft_left_redir_case(char *prompt, char *new_prompt, int *i, int *j, int state)
+char	*ft_insert_space(char *prompt)
 {
-    if (!prompt || !new_prompt || !i || !j)
-        return (0);
-    if (prompt[*j + 1] == '>')
-    {
-        state = 1;
-        new_prompt[*i] = prompt[*j];
-        (*i)++;
-        new_prompt[*i] = ' ';
-        (*i)++;
-        new_prompt[*i] = '>';
-        (*j)++;
-        if (prompt[*j+1] && prompt[*j+1] == '>')
-        {
-            (*i)++;
-            new_prompt[*i] = '>';
-            (*i)++;
-            new_prompt[*i] = ' ';
-            (*j)++;
-        }
-        else
-        {
-            (*i)++;
-            new_prompt[*i] = ' ';
-        }
-    }
-    return (state);
-}
-char  *ft_insert_space(char *prompt)
-{
-    int i;
-    int j;
-    int state;
-    char check1;
-    char check2;
-    char check3;
-    char *new_prompt;
+	int		index[2];
+	int		state;
+	char	*new_prompt;
 
-    i = 0;
-    j = 0;
-    new_prompt = malloc(strlen(prompt) + ft_count_space(prompt) + 10);
-    while(prompt[j]!='\0')
-    {
-        check1 = prompt[j];
-        check3 = new_prompt[i];
-        state = 0;
-        state = ft_pipe_case(prompt, new_prompt, &i, &j, state);
-        state = ft_right_redir_case(prompt, new_prompt, &i, &j, state);
-        state = ft_left_redir_case(prompt, new_prompt, &i, &j, state);
-        if (state == 0)
-        {
-            new_prompt[i] = prompt[j];
-        }
-        check3 = new_prompt[i];
-        i++;
-        j++;
-    }
-    new_prompt[i] = '\0';
-    return (new_prompt);
+	index[0] = 0;
+	index[1] = 0;
+	new_prompt = malloc(strlen(prompt) + ft_count_space(prompt) + 10);
+	while (prompt[index[1]] != '\0')
+	{
+		state = 0;
+		state = ft_pipe_case(prompt, new_prompt, index, state);
+		state = ft_right_redir_case(prompt, new_prompt, index, state);
+		state = ft_left_redir_case(prompt, new_prompt, index, state);
+		if (state == 0)
+		{
+			new_prompt[index[0]] = prompt[index[1]];
+		}
+		index[0]++;
+		index[1]++;
+	}
+	new_prompt[index[0]] = '\0';
+	return (new_prompt);
 }
 
 // int main()
 // {
-//     char *test = "l<outgile";
+//     char *test = "echo ls-la|cat grep l<outfile";
 //     printf("%zu\n", ft_count_space(test));
 //     char *new_test = ft_insert_space(test);
 
 //     printf("%s", new_test);
+// 	free(new_test);
 // }
--- src/tokenizer/tokenizer.h
+++ src/tokenizer/tokenizer.h
@@ -1,2 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zchagar <zchagar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 18:27:59 by zchagar           #+#    #+#             */
+/*   Updated: 2024/12/21 18:30:19 by zchagar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TOKENIZER_H
+# define TOKENIZER_H
+
 char	**ft_split(char const *s, char c);
-char  *ft_insert_space(char *prompt);
+size_t	ft_count_space(char *prompt);
+char	*ft_insert_space(char *prompt);
+
+#endif
