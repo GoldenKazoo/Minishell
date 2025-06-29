94df6ae61e3d9fb079bd927fb7b4b1278232554e
GoldenKazoo <goldenkazoomail@gmail.com>
2025-06-29 12:00:00 +0200

    fixed handlersig

--- src/signaux/handler.c
+++ src/signaux/handler.c
@@ -3,10 +3,11 @@
 #include <readline/history.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "minishell.h"
 
 static void	handle_sigint(int signo)
 {
-	g_signal = signo;
+	g_last_exit_status = signo;
 	write(STDOUT_FILENO, "\n", 1);
 	rl_replace_line("", 0);
 	rl_on_new_line();
@@ -15,7 +16,7 @@ static void	handle_sigint(int signo)
 
 static void	handle_sigquit(int signo)
 {
-	g_signal = signo;
+	g_last_exit_status = signo;
 }
 
 void	setup_signals(void)
